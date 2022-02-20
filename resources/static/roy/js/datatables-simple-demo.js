1a4b440da03736775b65f6b5db7eb101c17948fc
roy-dai <79895498+roy-dai@users.noreply.github.com>
2022-02-20 10:56:26 +0800

    Delete resources directory

--- resources/static/roy/js/datatables-simple-demo.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
-    const datatablesSimple = document.getElementById('datatablesSimple');
-    if (datatablesSimple) {
-    	console.log("test");
-        new simpleDatatables.DataTable(datatablesSimple);
-    }
-});
